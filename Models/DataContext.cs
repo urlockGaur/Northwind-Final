472a0be6d3d6956e6b61183d6404174c34647d33
jgrissom <jgrissom91@gmail.com>
2024-03-04 09:12:55 -0600

    [4.01a] Modify Schema - Add Discount class - Add Discounts DbSet to db context

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -6,4 +6,5 @@ public class DataContext : DbContext
 
   public DbSet<Product> Products { get; set; }
   public DbSet<Category> Categories { get; set; }
+  public DbSet<Discount> Discounts { get; set; }
 }
